20faf593f4d8082437a566e723c9a39548c71f62
olyuninv <olyuninv@tcd.ie>
2018-12-15 00:48:25 +0000

    Added dataset creation (reshuffle, divide into data and labels)

--- CNNForDigits/CNNForAnimals.py
+++ CNNForDigits/CNNForAnimals.py
@@ -1,13 +1,13 @@
-import tensorflow as tf
-from tensorflow import keras
-from keras.datasets import mnist
-from keras.preprocessing.image import ImageDataGenerator 
-from keras.utils import np_utils
-from keras import backend as K
+#import tensorflow as tf
+#from tensorflow import keras
+#from keras.datasets import mnist
+#from keras.preprocessing.image import ImageDataGenerator 
+#from keras.utils import np_utils
+#from keras import backend as K
 
-from mnist import MNIST
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 import time
 
 import sys
@@ -18,6 +18,27 @@ from Data import DataReader
 # =====================================================================
 
 if __name__ == '__main__':
-    data = DataReader.readImages('../../Data/n02118333/', 200, True)
+    dataLion = DataReader.readImages('../../Data/n02118333/', 0, 200, True)
+    dataFox = DataReader.readImages('../../Data/n02129165/', 1, 200, True)
+    dataTortoise = DataReader.readImages('../../Data/n01669191/', 2, 200, True)
+
+    # Keep first 1200
+    dataLion = dataLion[:1200]
+    dataFox = dataFox[:1200]
+    dataTortoise = dataTortoise[:1200]
+
+    dataset = np.concatenate([dataLion, dataFox, dataTortoise])
+    random.shuffle(dataset)
+
+    train_x = list()
+    train_y = list()
     
-    printf("Successfully loaded data")
+    # split into x and y
+    for (x, y) in dataset:
+        train_x.append(x)
+        train_y.append(y)
+
+    train_x = np.asarray(train_x)
+    train_y = np.asarray(train_y)
+    
+    print("Successfully loaded data")
--- Common/Data.py
+++ Common/Data.py
@@ -4,17 +4,28 @@ import os
 
 class DataReader():
 
-    def readImages(folderPath, resizeSize, ifBW):
+    def readImages(folderPath, label, resizeSize, ifBW):
         data = list()
 
+        min_size_image = 200
+                
         for file in os.listdir(folderPath):
             img = Image.open(os.path.join(folderPath, file))
             img = img.resize((resizeSize, resizeSize), Image.LANCZOS)
 
-            #if ifBW:
+            if ifBW:
+                img = img.convert('L')
             
             np_img = np.array(img)
-            data.append(np_img)
+            
+            if np_img.shape[0] < min_size_image: 
+                min_size_image = np_img.shape[0]
+            
+            if np_img.shape[1] < min_size_image: 
+                min_size_image = np_img.shape[1]
+
+            data.append((np_img, label))
         
+        print ("Min image dimension size: %i" % min_size_image)
         np_data = np.asarray(data)
-        return np.asarray(np_data)
+        return np_data
